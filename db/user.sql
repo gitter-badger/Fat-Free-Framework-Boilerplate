39cf508091ba4cff8e4f478f221a5590c3808836
Maxim Zavitaev <vskweb@gmail.com>
2015-06-13 01:31:59 +0300

    Язык пользователя в БД

--- db/user.sql
+++ db/user.sql
@@ -4,6 +4,7 @@ CREATE TABLE `user` (
   `email`        VARCHAR(64) NULL     DEFAULT NULL,
   `username`     VARCHAR(32) NULL     DEFAULT NULL,
   `password`     CHAR(60)    NULL     DEFAULT NULL,
+  `language`     VARCHAR(5)  NULL     DEFAULT NULL,
   `created_date` DATETIME    NOT NULL,
   `deleted_date` DATETIME    NULL     DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -13,6 +14,6 @@ CREATE TABLE `user` (
   COLLATE = 'utf8_general_ci'
   ENGINE = InnoDB;
 
-INSERT INTO `user` (`id`, `login`, `email`, `username`, `password`, `created_date`, `deleted_date`) VALUES
-  (1, 'admin', 'admin@mvc.local', 'Admin', '$2y$10$7ftcJlx6FSfM6hOSKbXbK.W68d.APjJmv7wx4LEKekcH/Si/q2h9C',
+INSERT INTO `user` (`id`, `login`, `email`, `username`, `password`, `language`, `created_date`, `deleted_date`) VALUES
+  (1, 'admin', 'admin@mvc.local', 'Admin', '$2y$10$7ftcJlx6FSfM6hOSKbXbK.W68d.APjJmv7wx4LEKekcH/Si/q2h9C', 'ru',
    '2015-06-13 00:34:01', NULL);
